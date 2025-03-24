bad4dc69541bceb52af8eff4af98281e96ec1477
Marucs <marcus.olssen@gmail.com>
2025-03-24 15:19:25 +0100

    Implemented kelvin_to_sRGB with interpolated values based on a lookup table.

--- tests/pHue.py
+++ tests/pHue.py
@@ -32,24 +32,60 @@ def get_light_with_name(lights: list[Light], name: str) -> Light | None:
     return None
 
 
-def xyY_to_sRGB(x: float, y: float, Y: int) -> ndarray[Any, dtype[Any]] | tuple[
-    int, int, int]:
+def xyY_to_sRGB(x: float, y: float, Y: int) -> ndarray:
+    if y == 0:
+        raise 'y can not be zero'
 
-    Y = Y / 254
+    Y = Y / 255
     X = x / y * Y
     Z = (1 - x - y) / y * Y
 
-    return (np.array(colour.XYZ_to_sRGB([X, Y, Z])) * 255).astype(int)
+    sRGB = colour.XYZ_to_RGB(np.array([X, Y, Z]), 'sRGB') * 255
+
+    sRGB = np.clip(sRGB.astype(int), 0, 255)
+
+    return sRGB
+
+
+def kelvin_to_sRGB(K: float) -> ndarray:
+    df = pd.read_csv('kelvin_table.csv')
+
+    def get_rgb(data):
+        R = data['R'].iloc[0]
+        G = data['G'].iloc[0]
+        B = data['B'].iloc[0]
+
+        return np.array([R, G, B])
+
+    try:
+        row = df.loc[df['kelvin'] == K]
+        return get_rgb(row)
+
+    except IndexError:
+        low = math.floor(K / 100) * 100
+        low_row = df.loc[df['kelvin'] == low]
+        lowRGB = get_rgb(low_row)
+
+        high = math.ceil(K / 100) * 100
+        high_row = df.loc[df['kelvin'] == high]
+        highRGB = get_rgb(high_row)
+
+        print(lowRGB, highRGB)
+
+        r = (high - K) / 100
+        interpolated = (1 - r) * lowRGB + r * highRGB
+
+        return np.round(interpolated).astype(int)
 
 
 def kelvin_cycle(light: Light) -> list[list[int | float]]:
     min_kelvin = 2000
-    max_kelvin = 6000
+    max_kelvin = 6500
 
     rgb_at_kelvin = []
 
-    kelvin_range = np.linspace(min_kelvin, max_kelvin, num=21, dtype=int)
-
+    kelvin_range = np.linspace(min_kelvin, max_kelvin, num=41, dtype=int)
+    #kelvin_range = range(2000, 6500+1)
     light.on = True
     time.sleep(0.5)
 
@@ -57,10 +93,12 @@ def kelvin_cycle(light: Light) -> list[list[int | float]]:
         light.colortemp_k = kelvin
 
         time.sleep(0.5)
-        print(f'kelvin is {light.colortemp_k}')
-        print('----')
         sRGB = xyY_to_sRGB(*light.xy, light.brightness)
-        rgb_at_kelvin.append([light.colortemp_k, *sRGB])
+        color = [f'\033[38;2;{sRGB[0]};{sRGB[1]};{sRGB[2]}m', '\033[0m']
+        print(f'kelvin is {color[0]}{kelvin}{color[1]}')
+        print('----')
+
+        rgb_at_kelvin.append([kelvin, *sRGB])
 
     light.on = False
 
@@ -113,10 +151,35 @@ kelvin_table_colour = {
     5814: (202, 194, 185),
     5988: (200, 195, 188)
 }
+table = {
+    2000: (255, 164, 5),
+    2200: (255, 178, 16),
+    2400: (255, 190, 29),
+    2600: (255, 199, 42),
+    2800: (255, 207, 55),
+    3000: (255, 214, 69),
+    3200: (255, 220, 82),
+    3400: (255, 225, 94),
+    3600: (255, 229, 107),
+    3800: (255, 232, 120),
+    4000: (255, 235, 131),
+    4200: (255, 238, 144),
+    4400: (255, 240, 155),
+    4600: (255, 242, 166),
+    4800: (255, 244, 177),
+    5000: (255, 246, 187),
+    5200: (255, 247, 198),
+    5400: (255, 248, 207),
+    5600: (255, 250, 215),
+    5800: (255, 251, 226),
+    6000: (255, 251, 233)
+}
+
 
 if __name__ == '__main__':
+    print(kelvin_to_sRGB(4630))
+    exit()
     id, ip, port = get_bridge_ip()
-
     bridge = Bridge(ip)
     bridge.connect()
 
