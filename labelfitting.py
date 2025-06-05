09d6e4d5559f8a89069619a01f9c15b801396b1b
Marcus Z. Olssen <marcus.olssen@gmail.com>
2025-06-05 15:05:29 +0200

    feat: Saved computation time by loading embeddings instead of recalculating them

--- labelfitting.py
+++ labelfitting.py
@@ -1,113 +1,118 @@
-import os
-import ast
-import torch
-import clip
-from PIL import Image
-from pathlib import Path
 from collections import Counter
-from tqdm import tqdm
 import json
-
-# Version that works for coil and our own data
-datasets = {"coil20": {}, "Dataset": {}, "processed": {}, "coil100": {}}
-
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f'Model is running on {device}', end='\n\n')
-model, preprocess = clip.load("ViT-L/14@336px", device)
-
-# Load ImageNet class names from txt
-with open("Imagenet_classes.txt", "r") as f:
-    class_dict = ast.literal_eval(f.read())
-imagenet_labels = [v.split(',')[0].strip() for v in class_dict.values()]
-print(f'All ImageNet labels are \n{imagenet_labels}', end='\n\n')
+import torch
+from tqdm import tqdm
+import clip
 
 
-# Load our object class names from json
-with open("object_labels.json", "r") as f:
-    object_labels = json.load(f)
-print(f'All object labels are: \n{object_labels}', end='\n\n')
-datasets["Dataset"] = object_labels
-datasets["processed"] = object_labels
+def get_image_embeddings(dataset_name: str) -> dict[str, dict[str, list]]:
+    """
+    Args:
+        dataset_name: Name of the dataset path. Could be "coil100"
 
+    Returns:
+        Dictionary of the "rot" json
 
-# Load coil100 object class names from json
-with open("COIL100_GPT_labels.json", "r") as f:
-    object_labels_coil100 = json.load(f)
-print(f'All coil100 object labels are: \n{object_labels_coil100}', end='\n\n')
-datasets["coil100"] = object_labels_coil100
+    """
+    with open(f'Embeddings/{dataset_name}.json', 'r', encoding='utf-8') as f:
+        embeddings = json.load(f)
+    return embeddings['rot']
 
-
-for dataset, label in datasets.items():
-    print(f'Currently running on {dataset}')
-
-    current_dataset = f'Data/{dataset}/rot'
-
-    labels = imagenet_labels.copy()
-    if len(label) > 0:
-        for values in label.values():
-            labels.extend(values)
-
-        labels = list(set(labels))
-
-    # Tokenize labelset using clip.tokenize:
-    labelset = torch.cat([clip.tokenize(f"a photo of a {label}") for label in labels]).to(device)
-
-    # Encode labelset embedding vectors
-    with torch.no_grad():
-        text_features = model.encode_text(labelset)
-        text_features /= text_features.norm(dim=-1, keepdim=True)
-
-    # Embed an image
-    def embed_image(image_path):
-        image = Image.open(image_path).convert("RGB")
-        image_input = preprocess(image).unsqueeze(0).to(device)
-        with torch.no_grad():
-            image_features = model.encode_image(image_input)
-            image_features /= image_features.norm(dim=-1, keepdim=True)
-        return image_features
-
-    # Main loop
-    fitted_labels = {}
-
-    # Get prediction scores and the ground truth label
-    obj_iterable = os.listdir(current_dataset)
-    for obj_name in tqdm(obj_iterable):
-        image_folder = Path(f"{current_dataset}/{obj_name}/").resolve()
-        images = os.listdir(image_folder)
-
-        predictions = []
-        for image in images:
-            image_path = os.path.join(image_folder, image)
-            image_features = embed_image(image_path)
-
-            # Compute similarity (100X comes from CLIP's git)
-            logits = 100 * image_features @ text_features.T
-            probs = torch.softmax(logits, dim=1)
-            top_idx = torch.argmax(probs, dim=1).item()
-            predicted_label = labels[top_idx]
-            predictions.append(predicted_label)
-
-        most_common_label = Counter(predictions).most_common(1)[0][0]
-        fitted_labels[obj_name] = most_common_label
-
-    print(f'{"Object:":<20} | Most common label:')
-    print('-' * (20*2 + 1))
-    for obj_name, most_common_label in fitted_labels.items():
-        print(f'{obj_name:<20} | {most_common_label}')
-    print() # Empty space at end
-
+def save_labels(dataset_name: str, fit_labels: dict, base_labels: list):
     # Saving the fitted labels for each object
-    with open(f'Data/{dataset}/GT_labels.json', 'w') as f:
-        f.write(json.dumps(fitted_labels, indent=4))
-    print(f'Successively saved label {dataset} to "Data/{dataset}/GT_labels_{dataset}.json"!')
+    with open(f'Data/{dataset_name}/GT_labels.json', 'w') as f:
+        f.write(json.dumps(fit_labels, indent=4))
+    print(f'Successively saved label {dataset_name} to "Data/{dataset_name}/GT_labels.json"!')
 
     # Saving the fitted labels as a complete label dataset
-    label_dataset = imagenet_labels.copy()
+    base_labels = base_labels.copy()
     for label in fitted_labels.values():
-        label_dataset.append(label) if label not in label_dataset else None
-    label_dict = {label: id for id, label in enumerate(label_dataset)}
+        base_labels.append(label) if label not in base_labels else None
+    label_dict = {label: id for id, label in enumerate(base_labels)}
 
-    with open(f'Data/{dataset}/labels.json', 'w') as f:
+    with open(f'Data/{dataset_name}/labels.json', 'w') as f:
         f.write(json.dumps(label_dict, indent=4))
-    print(f'Successively saved label dataset {dataset} to "Data/{dataset}/labels.json"!', end='\n\n')
+    print(f'Successively saved label dataset {dataset_name} to "Data/{dataset_name}/labels.json"!', end='\n\n')
+
+
+if __name__ == '__main__':
+    # Version that works for coil and our own data
+    datasets = {"coil20": {}, "Dataset": {}, "processed": {}, "coil100": {}}
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f'Model is running on {device}', end='\n\n')
+    model, preprocess = clip.load("ViT-L/14@336px", device)
+
+    # Load ImageNet class names from txt
+    with open("Imagenet_classes.json", "r") as f:
+        class_dict = json.load(f)
+    imagenet_labels = []
+    for label in class_dict.values():
+        label_string: str = label[0]
+        first_label = label_string.split(", ")[0].strip()
+
+        imagenet_labels.append(first_label)
+    print(f'All ImageNet labels are \n{imagenet_labels}', end='\n\n')
+
+    # Load our object class names from json
+    with open("object_labels.json", "r") as f:
+        object_labels = json.load(f)
+    print(f'All object labels are: \n{object_labels}', end='\n\n')
+    datasets["Dataset"] = object_labels
+    datasets["processed"] = object_labels
+
+    # Load coil100 object class names from json
+    with open("COIL100_GPT_labels.json", "r") as f:
+        object_labels_coil100 = json.load(f)
+    print(f'All coil100 object labels are: \n{object_labels_coil100}', end='\n\n')
+    datasets["coil100"] = object_labels_coil100
+
+
+
+    for dataset, label in datasets.items():
+        print(f'Currently running on {dataset}')
+
+        labels = imagenet_labels.copy()  # Reset labels to be ImageNet labels
+        if len(label) > 0:  # If the current dataset has any labels attached, then add them to the labels list
+            for values in label.values():
+                labels.extend(values)
+
+            labels = list(set(labels))
+
+        # Tokenize labelset using clip.tokenize:
+        labelset = torch.cat([clip.tokenize(f"a photo of a {label}") for label in labels]).to(device)
+
+        # Encode labelset embedding vectors
+        with torch.no_grad():
+            text_features = model.encode_text(labelset)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+
+        # Main loop
+        fitted_labels = {}
+
+        # Get prediction scores and the ground truth label
+        image_embeddings = get_image_embeddings(dataset)
+        for obj_name, embeddings in tqdm(image_embeddings.items()):
+            predictions = []
+            for embed in embeddings.values():
+                embed = torch.tensor(embed, device=device, dtype=text_features.dtype)
+
+                # Compute similarity with a temperature of 0.01
+                temp = 1/100
+                logits = embed @ text_features.T / temp
+                probs = torch.softmax(logits, dim=-1)
+                top_idx = torch.argmax(probs, dim=-1).item()
+
+                predicted_label = labels[top_idx]
+                predictions.append(predicted_label)
+
+            most_common_label = Counter(predictions).most_common(1)[0][0]
+            fitted_labels[obj_name] = most_common_label
+
+        print(f'{"Object:":<20} | Most common label:')
+        print('-' * (20 * 2 + 1))
+        for obj_name, most_common_label in fitted_labels.items():
+            print(f'{obj_name:<20} | {most_common_label}')
+        print()  # Empty space at end
+
+        save_labels(dataset, fitted_labels, imagenet_labels)
